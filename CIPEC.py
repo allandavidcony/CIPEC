74ca4ed2524be47b2839299aa6bc1d1108cb594f
giancamilo <gian.fis@gmail.com>
2025-02-24 23:45:26 +0400

    added efficient computation of basis and display cosmetics

--- CIPEC.py
+++ CIPEC.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sympy import Matrix
-from IPython.display import display
+from IPython.display import display, Math, Latex
 
 
 from qibo import quantum_info as qi
@@ -23,7 +23,21 @@ import csv
 ############################################## GENERAL ###############################################
 def disp(a=np.eye(2), rnd: int = 3):
     display(Matrix(a.round(rnd)))
-    
+
+
+def fancy_display(a=np.eye(2), lhs='', rnd: int = 3):
+    if isinstance(a, np.ndarray):
+        if len(lhs)>0:
+            return display(Latex(rf'${lhs}='+Matrix(a.round(rnd))._repr_latex_()[15:-1]+'$'))
+        else:
+            return display(Matrix(a.round(rnd)))
+    else:
+        if len(lhs)>0:
+            return display(Latex(rf'${lhs}={a.round(rnd)}$'))
+        else:
+            return display(Latex(rf'${np.round(a,rnd)}$'))
+
+
 
 def write(data,filename):
     with open(filename, mode='w', newline='') as file:
@@ -74,16 +88,18 @@ def clifford_T_channels(n_qubits, include_T=True):
     S = gates.S(0).matrix() 
     T = gates.T(0).matrix() 
     CX = gates.CNOT(0,1).matrix() 
+    # XC = gates.CNOT(1,0).matrix() 
     gateset_dict = {'I':I, 'H': H, 'S': S, 'T': T, 'CX': CX}
      
     # corresponding channels in Choi rep.
     if n_qubits == 1:
-        unitary_gates = {k:v for k,v in gateset_dict.items() if k!='CX'}
+        unitary_gates = {k:v for k,v in gateset_dict.items() if k not in set(['CX'])}
 
     if n_qubits == 2:
         keys = itertools.product(['I','H','S','T'],['I','H','S','T'])
         unitary_gates = {''.join(k):np.kron(gateset_dict[k[0]],gateset_dict[k[1]]) for k in keys}
         unitary_gates['CX'] = CX
+        # unitary_gates['XC'] = XC
                 
     if not include_T:
         has_T = [k for k in list(unitary_gates.keys()) if 'T' in k]
@@ -183,50 +199,78 @@ def noiseless_basis(nqubits, include_T=True):
         CPTP_dim = 2**(4*nqubits) - 2**(2*nqubits) + 1
         print(f'dim(CPTP)={CPTP_dim}\n')
 
-        # start composing only unitary channels
-        keys = list(unitary_channels.keys())
-        composite_keys = []
-        B = {}
-        rank = 0
+        # Sequences of unitary channels
+        letters = list(unitary_channels.keys())
+        print(f'Using letters {letters}')
+        
+        B = {tuple([k]):v for k,v in list(unitary_channels.items())} # initiate basis with all words of L=1 since they are LI
+        accepted_keys = [tuple([k]) for k in list(unitary_channels.keys())] 
+        G = gram_matrix(list(B.values()))
+        rank = np.linalg.matrix_rank(G)
 
-        # words of length 1
-        for k in list(itertools.product(keys)):
-            composite_keys.append(tuple([k[0]]))
-        # words of length 2
-        for k in list(itertools.product(keys,keys)):
-            composite_keys.append((k[0],k[1]))
-        # words of length 3
-        for k in list(itertools.product(keys,keys,keys)):
-            composite_keys.append((k[0],k[1],k[2]))
-        # words of length 4
-        for k in list(itertools.product(keys,keys,keys,keys)):
-            composite_keys.append((k[0],k[1],k[2],k[3]))
-
-        print(f"Starting sequences of unitaries. There are {len(composite_keys)} candidate words up to L=4 to try.")
-
-        composite_keys = tqdm(composite_keys)
-        G = np.empty([0,0], dtype='complex_')
-        for k in composite_keys:
-            new_channel = qi.choi_to_liouville(unitary_channels[k[0]])
-            if len(k) > 1:
+        Lstar = (4 if nqubits==1 else 5) # then start checking words of L>1
+        for L in range(2,Lstar+1):
+            trial_keys = [tuple(list(k)+[l]) for k in accepted_keys for l in letters] # time saving! only add new letter to previously accepted words 
+            print(f"Starting sequences of L={L}. There are {len(trial_keys)} to try.")   
+            trial_keys = tqdm(trial_keys)     
+            accepted_keys = []
+            for k in trial_keys: # compose all channels in k to get new channel, then check if adding it to G increases the rank
+                new_channel = qi.choi_to_liouville(unitary_channels[k[0]])
                 for i in range(1,len(k)):
                     new_channel = qi.choi_to_liouville(unitary_channels[k[i]])@new_channel
-            new_channel = qi.liouville_to_choi(new_channel)
-            GU = update_gram_matrix(G, list(B.values()), new_channel)
-            if np.linalg.matrix_rank(GU) > rank:
-                rank += 1
-                B[k] = new_channel
-                G = GU
-                if rank == CPTP_dim-len(nonunitary_channels):
-                    print("Success!")
-                    break
-            else:
-                pass
-            composite_keys.set_description(f'Current rank(G) = {rank}')
+                new_channel = qi.liouville_to_choi(new_channel)
+                GU = update_gram_matrix(G, list(B.values()), new_channel)
+                if np.linalg.matrix_rank(GU) > rank:
+                    rank += 1
+                    B[k] = new_channel
+                    G = GU
+                    accepted_keys.append(k)
+                    if rank == CPTP_dim-len(nonunitary_channels):
+                        print("Success!")
+                        break
+                else:
+                    pass        
+                trial_keys.set_description(f'Current rank(G) = {rank}')                  
+
+        ### Exhaustive search over all the words (very slow)
+        # # words of length 1
+        # for k in list(itertools.product(keys)):
+        #     composite_keys.append(tuple([k[0]]))
+        # # words of length 2
+        # for k in list(itertools.product(keys,keys)):
+        #     composite_keys.append((k[0],k[1]))
+        # # words of length 3
+        # for k in list(itertools.product(keys,keys,keys)):
+        #     composite_keys.append((k[0],k[1],k[2]))
+        # # words of length 4
+        # for k in list(itertools.product(keys,keys,keys,keys)):
+        #     composite_keys.append((k[0],k[1],k[2],k[3]))
+
+        # print(f"Starting sequences of unitaries. There are {len(composite_keys)} candidate words up to L=4 to try.")
+
+        # composite_keys = tqdm(composite_keys)
+        # G = np.empty([0,0], dtype='complex_')
+        # for k in composite_keys:
+        #     new_channel = qi.choi_to_liouville(unitary_channels[k[0]])
+        #     if len(k) > 1:
+        #         for i in range(1,len(k)):
+        #             new_channel = qi.choi_to_liouville(unitary_channels[k[i]])@new_channel
+        #     new_channel = qi.liouville_to_choi(new_channel)
+        #     GU = update_gram_matrix(G, list(B.values()), new_channel)
+        #     if np.linalg.matrix_rank(GU) > rank:
+        #         rank += 1
+        #         B[k] = new_channel
+        #         G = GU
+        #         if rank == CPTP_dim-len(nonunitary_channels):
+        #             print("Success!")
+        #             break
+        #     else:
+        #         pass
+        #     composite_keys.set_description(f'Current rank(G) = {rank}')
 
         print(f"Achieved rank(G)={rank}\n")
 
-        # now add state prep channels
+        # now fill the non-unital directions by adding state prep channels
         keys = [tuple([k]) for k in list(nonunitary_channels.keys())]
         print(f"Adding now state prep channels")
         keys = tqdm(keys)
@@ -307,12 +351,12 @@ def apply_noise_to_basis(B,noise_model):
 def params_basic_2q(params_basic):
 
     """ 
-    Input: depolarizing parameters for H, S, T, CX, XC, Px, Py, Pz
-    Output: depolarizing parameters for CX, XC and tensor products of [I,H,S,T] and of [I,Px,Py,Pz]
+    Input: depolarizing parameters for H, S, T, CX, Px, Py, Pz
+    Output: depolarizing parameters for CX and tensor products of [I,H,S,T] and of [I,Px,Py,Pz]
     """
     params = {}
     
-    for gate in ('CX','XC'):
+    for gate in set(['CX']):
         params[gate] = params_basic[gate]
     for gate in itertools.product(['I','H','S','T'],['I','H','S','T']):
         params[gate[0]+gate[1]] = params_basic[gate[0]]+params_basic[gate[1]]
